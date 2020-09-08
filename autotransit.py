341369363a25e51dc0899df9cdc2012fea4c5426
Jason Rowe <Jason.Rowe@ubishops.ca>
2020-09-08 15:38:13 -0400

    Layout of goals

--- autotransit.py
+++ autotransit.py
@@ -1,6 +1,72 @@
+import sys
+
 import numpy as np
 from astroquery.mast import Observations, Catalogs
 from astropy.io import fits
+
+import json
+
+try: # Python 3.x
+    from urllib.parse import quote as urlencode
+    from urllib.request import urlretrieve
+except ImportError:  # Python 2.x
+    from urllib import pathname2url as urlencode
+    from urllib import urlretrieve
+
+try: # Python 3.x
+    import http.client as httplib
+except ImportError:  # Python 2.x
+    import httplib
+
+def mastQuery(request):
+
+    server='mast.stsci.edu'
+
+    # Grab Python Version
+    version = ".".join(map(str, sys.version_info[:3]))
+
+    # Create Http Header Variables
+    headers = {"Content-type": "application/x-www-form-urlencoded",
+        "Accept": "text/plain",
+        "User-agent":"python-requests/"+version}
+
+    # Encoding the request as a json string
+    requestString = json.dumps(request)
+    requestString = urlencode(requestString)
+
+    # opening the https connection
+    conn = httplib.HTTPSConnection(server)
+
+    # Making the query
+    conn.request("POST", "/api/v0/invoke", "request="+requestString, headers)
+
+    # Getting the response
+    resp = conn.getresponse()
+    head = resp.getheaders()
+    content = resp.read().decode('utf-8')
+
+    # Close the https connection
+    conn.close()
+
+    return head,content
+
+def ticAdvancedSearch(id):
+    request = {"service":"Mast.Catalogs.Filtered.Tic",
+                "format":"json",
+                "params":{
+                "columns":"*",
+                "filters":[
+                    {"paramName":"id",
+                        "values":[{"min":id,"max":id}]}]
+                        #"values":[{261136679}]}]
+                }}
+
+    headers,outString = mastQuery(request)
+    outData = json.loads(outString)
+
+    return outData
+
+
 
 def get_tess_data(u_ticid,max_flag=16,out_dir='./download'):
     """Given a TIC-ID, return time,flux,ferr,itime
